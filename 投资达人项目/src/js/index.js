08596d9ec9a0e3a2ac2d8c1085afcd9aa0ce8cfa
xiezhi5233 <xiezhi5233@126.com>
2017-11-02 08:30:15 +0800

    投资达人项目3.0

---
+++ 投资达人项目/src/js/index.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+require ("font-awesome-webpack");//font-awesome-webpack字体图标
+import '../css/Normalize.css';//统一浏览器默认样式
+import 'bootstrap/dist/css/bootstrap.css'//bootstrap包含了字体图标,但是数量太少
+import 'Swiper/dist/css/swiper.css';//bootstrap轮播插件
+import '../css/index.css';//主要css!!!!!!!!!!!!!!!!!!!!!!
+import 'bootstrap/dist/js/bootstrap.js'//bootstrap.js
+import Swiper from 'Swiper/dist/js/swiper.js';//bootstrap轮播插件js
+
+
+
---
+++ 投资达人项目/webpack.config.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	//entry:  __dirname + "/src/js/index.js",//唯一入口文件,值可以是数组
+	entry:{
+		index:__dirname + "/src/js/index.js"
+	},
+	//多入口建议使用对象格式
+	output: {
+		path: __dirname + "/public",
+		//打包后的文件存放的路径
+		filename:"js/[name]-[hash].js"
+		//打包后输出文件的name
+		//xxx-[hash].xx是哈希值,一段随机字符串
+		//[name]可以自动独打包对象中的每一个文件
+	},
+	devServer:{
+		contentBase:"./pablic",
+		inline:true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader",
+					publicPath:"../"
+				})
+			},
+			{
+				test: /\.(png|jpg|gif|jpeg)$/,
+				use: [{
+					loader: 'url-loader?limit=8192&name=img/[hash:8].[name].[ext]'
+				}]
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-withimg-loader'
+			 },
+			{
+				test: /\.(woff|woff2|svg|eot|xttf|ttf)\??.*$/,
+				use: [
+					{
+						loader: 'url-loader?name=fonts/[name].[md5:hash:hex:7].[ext]'
+					}
+				]
+			}
+		]
+	},
+	 plugins:[
+			new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'}),
+			new ExtractTextPlugin("./css/[name].css"),
+			new HtmlWebpackPlugin({
+				template:'./src/index.html',
+				filename:'./index.html',
+				chunks:['index'],
+				hash:true,
+				minify:{
+					removeAttributeQuotes: true, // 移除属性的引号
+					caseSensitive: false, //是否大小写敏感
+					removeComments:true, // 去除注释
+					removeEmptyAttributes:true, // 去除空属性
+					collapseWhitespace: true //是否去除空格
+				}
+			})
+	]
+}
+
